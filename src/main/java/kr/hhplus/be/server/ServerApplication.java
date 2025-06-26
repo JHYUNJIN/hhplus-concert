0de8f2fd12b83ae822ca6e011d03ef9f30193e24
jhyunjin <wjdguswls852@naver.com>
2025-06-27 04:36:28 +0900

    Fix: JPA 리포지토리 스캔 설정 오류 수정
    - `kr.hhplus.be.infrastructure.persistence` -> `kr.hhplus.be.server.infrastructure.persistence`
    - ServerApplication 위치 수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.infrastructure;
+package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/kr/hhplus/be/server/infrastructure/configuration/JpaConfig.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/configuration/JpaConfig.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableJpaAuditing // JPA Auditing 기능 활성화 (created_at, updated_at 자동 관리)
 // Clean Architecture 구조에 맞게 basePackages를 변경합니다.
 // JpaRepository 인터페이스 구현체들이 위치할 새로운 경로를 정확히 명시합니다.
-@EnableJpaRepositories(basePackages = "kr.hhplus.be.infrastructure.persistence") 
+@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.infrastructure.persistence")
 public class JpaConfig {
 
     // PlatformTransactionManager 빈을 명시적으로 정의합니다.
