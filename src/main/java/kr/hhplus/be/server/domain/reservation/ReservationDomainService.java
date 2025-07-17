d1fab5d5289b523f9446bed82d0cbc425c19139a
jhyunjin <wjdguswls852@naver.com>
2025-07-18 00:24:42 +0900

    CHORE: 이전 커밋 누락 내용 추가 - 중복된 좌석 유효성 검사 제거

--- src/main/java/kr/hhplus/be/server/domain/reservation/ReservationDomainService.java
+++ src/main/java/kr/hhplus/be/server/domain/reservation/ReservationDomainService.java
@@ -1,22 +1,21 @@
 package kr.hhplus.be.server.domain.reservation;
 
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
-import kr.hhplus.be.server.domain.concertDate.ConcertDate;
-import kr.hhplus.be.server.domain.seat.Seat;
-import kr.hhplus.be.server.domain.payment.Payment;
 import kr.hhplus.be.server.common.exception.CustomException;
 import kr.hhplus.be.server.common.exception.enums.ErrorCode;
+import kr.hhplus.be.server.domain.concertDate.ConcertDate;
+import kr.hhplus.be.server.domain.seat.Seat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationDomainService {
 
+    // 좌석 예약 처리
     public ReservationDomainResult processReservation(ConcertDate concertDate, Seat seat, UUID userId) throws CustomException {
-        validateSeatAvailable(seat);
+//        validateSeatAvailable(seat); // reserve() 메소드 내부로 이동, 고튜터님 피드백 적용
         validateConcertDateDeadline(concertDate);
 
         Seat 		reservedSeat 	= 	seat.reserve();
@@ -25,10 +24,10 @@ public class ReservationDomainService {
         return new ReservationDomainResult(reservedSeat, reservation);
     }
 
-    private void validateSeatAvailable(Seat seat) throws CustomException {
-        if (!seat.isAvailable())
-            throw new CustomException(ErrorCode.ALREADY_RESERVED_SEAT);
-    }
+//    private void validateSeatAvailable(Seat seat) throws CustomException {
+//        if (!seat.isAvailable())
+//            throw new CustomException(ErrorCode.ALREADY_RESERVED_SEAT);
+//    }
 
     private void validateConcertDateDeadline(ConcertDate concertDate) throws CustomException {
         if (!concertDate.checkDeadline())
